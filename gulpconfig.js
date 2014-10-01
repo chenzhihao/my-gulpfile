97b436d0f4bc8cc14ff1710633d66d0d45fcce1d
Jerry <cabtravis@gmail.com>
2014-10-01 14:50:54 +0800

    Refactor code

--- gulpconfig.js
+++ gulpconfig.js
@@ -1,17 +1,16 @@
-module.exports = (function () {
-  var src = 'src/'
-    , dest = 'htdocs/';
-  return {
-    script: {
-      src: src + 'scripts/**/*.js ',
-      vendor: ['jquery.js'],
-      dest: dest + 'javascript'
-    },
-    style: {
-      src: src + '/stylesheets/**/*.scss',
-      vendor: src + '/stylesheets/vendor/**/*.css',
-      dest: dest + 'css'
-    }
+var src = 'src/'
+  , dest = 'htdocs/';
+
+module.exports = {
+  script: {
+    src: src + 'scripts/**/*.js ',
+    vendor: ['jquery.js'],
+    dest: dest + 'javascript'
+  },
+  style: {
+    src: src + '/stylesheets/**/*.scss',
+    vendor: src + '/stylesheets/vendor/**/*.css',
+    dest: dest + 'css'
   }
-})();
+};
 
